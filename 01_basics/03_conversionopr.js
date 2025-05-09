2bcb6b7c416df689dc3452cd7c769490a97daf1f
priya singh <priyakushwaha83036@gmail.com>
2025-05-09 22:23:21 +0530

    conversion of dataTypes

---
+++ 01_basics/03_conversionopr.js
@@ -0,0 +1,22 @@
+let score="33abc"
+//console.log(typeof score);
+//console.log(typeof (score));
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);
+//"33" convert ho jaayega number me = 33
+//"33abc" == NaN(not a number)
+//true=1,false=0
+let isLoggedIn = ""
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+//1==true 0==false
+//"" => false
+//"priya" => true
+
+let someNumber = 25
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
