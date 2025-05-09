63cc117e8a578819ad99011b31b1b9481acf2445
priya singh <priyakushwaha83036@gmail.com>
2025-05-09 22:51:05 +0530

    string to number conversion

--- 01_basics/03_conversionopr.js
+++ 01_basics/03_conversionopr.js
@@ -16,7 +16,39 @@ let booleanIsLoggedIn=Boolean(isLoggedIn);
 
 let someNumber = 25
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+//********************operations**********************  
+
+let value=46
+let negValue=-value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="priya"
+let str2="singh"
+let str3 = str1+str2
+//console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+console.log(true);
+console.log(+true);// this is wrong way to write code
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
 
 
