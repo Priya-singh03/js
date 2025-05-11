64837b9ca0b61bf18d550505aafc3dd477d6c33d
priya singh <priyakushwaha83036@gmail.com>
2025-05-12 00:17:21 +0530

    nums and math

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,41 @@
+const score=400
+//console.log(score);
+const balance= new Number(100)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(typeof balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+
+const otherNumber= 345.89338
+// console.log(otherNumber.toPrecision(2));
+// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(3));
+
+
+const hundreds= 100000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++++Math++++++++++++++++++++++//
+
+// console.log(Math);
+// console.log(Math.abs(-45));
+// console.log(Math.round(4.9575));
+// console.log(Math.ceil(4.9575));
+// console.log(Math.floor(4.9575));
+// console.log(Math.min(4,8,2,5));
+// console.log(Math.max(4,8,2,5));
+
+
+console.log(Math.random()); // hmesha 0 and 1 ke bich ki value deti hai
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);// 0 wala case remove krne ke liye 1 add kr liye kyuki hmlogo ki 1 se upr value chahiye hi chahiye
+
+
+const min=20
+const max=50
+console.log(Math.floor((Math.random() * (max- min +1)) +min ));
+
