8ad04db5b55cd92bce98dc99236fac0b26a2283a
priya singh <priyakushwaha83036@gmail.com>
2025-05-14 23:29:17 +0530

    how to work switch statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,47 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month =3
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("febuary");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("april"); 
+        break;
+}
+const day = "thur"
+switch (day) {
+    case "mon":
+        console.log("monday");
+        break;
+    case "tue":
+        console.log("tuesday");
+        break;
+    case "wed":
+        console.log("wednesday");
+        break;
+    case "thur":
+        console.log("thursday");
+        break;
+
+    default:
+        console.log("not a valid day"); 
+        break;
+}
